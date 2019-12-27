1e0358da89381e8ffb0f821f3dad0b544e194edf
Namita <namiita16july@gmail.com>
2019-12-27 21:32:45 +0530

    Initial commit package

---
+++ week3/src/week3package/week3class.java
@@ -0,0 +1,31 @@
+package week3package;
+
+public class week3class {
+	static int a=10;
+	static int b=5;
+	
+	public static void Add() {
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+	public static void Mul() {
+		int c=a*b;
+		System.out.println(c);
+	}
+	public static void Sub() {
+		int c=a-b;
+		System.out.println(c);
+	}
+	
+
+	public static void main(String[] args) {
+		
+		Add();
+		Sub();
+		Mul();
+		
+
+	}
+
+}
